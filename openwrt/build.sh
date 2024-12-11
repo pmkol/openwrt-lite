29d7b67db55d4614ed7bdd29c16359e47b413c50
MapleCool <37971849+pmkol@users.noreply.github.com>
2024-12-11 13:16:49 +0800

    Update build.sh

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -205,9 +205,9 @@ echo -e "\n${GREEN_COLOR}Patching ...${RES}\n"
 
 # scripts
 curl -sO https://$mirror/openwrt/scripts/00-prepare_base.sh
-export mirror=raw.githubusercontent.com/pmkol/test/build
+# export mirror=raw.githubusercontent.com/pmkol/test/build
 curl -sO https://$mirror/openwrt/scripts/01-prepare_base-mainline.sh
-export mirror=raw.githubusercontent.com/pmkol/openwrt-lite/main
+# export mirror=raw.githubusercontent.com/pmkol/openwrt-lite/main
 curl -sO https://$mirror/openwrt/scripts/02-prepare_package.sh
 curl -sO https://$mirror/openwrt/scripts/03-convert_translation.sh
 curl -sO https://$mirror/openwrt/scripts/04-fix_kmod.sh
@@ -217,9 +217,9 @@ curl -sO https://$mirror/openwrt/scripts/99_clean_build_cache.sh
 chmod 0755 *sh
 [ "$(whoami)" = "runner" ] && group "patching openwrt"
 bash 00-prepare_base.sh
-export mirror=raw.githubusercontent.com/pmkol/test/build
+# export mirror=raw.githubusercontent.com/pmkol/test/build
 bash 01-prepare_base-mainline.sh
-export mirror=raw.githubusercontent.com/pmkol/openwrt-lite/main
+# export mirror=raw.githubusercontent.com/pmkol/openwrt-lite/main
 bash 02-prepare_package.sh
 bash 03-convert_translation.sh
 bash 04-fix_kmod.sh
